1e591652e9c2a76604724ba32f8a9be8add77fc5
Statanly <43928400+Statanly@users.noreply.github.com>
2022-11-15 14:29:54 +0300

    Add reestr page

---
+++ Statanly.Web/Controllers/InfoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Statanly.Web.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Reestr()
+        {
+            return View();
+        }
+        public FileResult Documentation()
+        {
+            return File("~/Content/Documentation.pdf", System.Net.Mime.MediaTypeNames.Application.Pdf);
+        }
+        public FileResult Code()
+        {
+            return File("~/Content/cvflow.zip", System.Net.Mime.MediaTypeNames.Application.Zip);
+        }
+        public FileResult Accreditation()
+        {
+            return File("~/Content/Accreditation.pdf", System.Net.Mime.MediaTypeNames.Application.Pdf);
+        }
+    }
+}
