4239c323b3ff9fc4c2b498493d3131156c9f492c
Statanly <43928400+Statanly@users.noreply.github.com>
2025-08-28 17:31:44 +0300

    some news

---
+++ Statanly.Web/Controllers/NewsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Statanly.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Granulometry()
+        {
+            return View();
+        }
+    }
+}
