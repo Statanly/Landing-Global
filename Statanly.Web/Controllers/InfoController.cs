cd1e3cb3c98aec649adbe870a3e53d232b01d291
Statanly <43928400+Statanly@users.noreply.github.com>
2025-10-21 22:53:57 +0300

    add news

--- Statanly.Web/Controllers/InfoController.cs
+++ Statanly.Web/Controllers/InfoController.cs
@@ -105,6 +105,10 @@ namespace Statanly.Web.Controllers
         {
             return File("~/Content/Agro Presentation.pdf", System.Net.Mime.MediaTypeNames.Application.Pdf);
         }
+        public FileResult Industry()
+        {
+            return File("~/Content/AI in industry.pdf", System.Net.Mime.MediaTypeNames.Application.Pdf);
+        }
         public FileResult Avia()
         {
             return File("~/Content/Avia.pdf", System.Net.Mime.MediaTypeNames.Application.Pdf);
